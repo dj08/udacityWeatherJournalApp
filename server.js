d203d1745d02e93178f1ae53cc53fb586b068f7d
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-24 18:53:04 -0500

    Add first cut listening function

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var app = express();
+const port = 8080;
+const express = require('express');
+const app = express();
 
 // get method needs first param as URL suffix and second method to
 // manipulate the response.
 app.get('/', (request, response) => {
     response.send('hello world');
-})
+});
+
+const server = app.listen(port, () => {
+    console.log(`Running on localhost: ${port}`);
+});
+
