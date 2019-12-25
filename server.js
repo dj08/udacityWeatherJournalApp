35a01ad2cf412302ae47ca118a0c77f5604a365c
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-25 10:42:01 -0500

    Add first cut server side code and package installation

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const port = 8080;
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Array to hold user data
+const appData = [];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 // get method needs first param as URL suffix and second method to
 // manipulate the response.
-app.get('/', (request, response) => {
-    response.send('hello world');
+// app.get('/', (request, response) => {
+//     response.send('hello world');
+// });
+
+app.get('/all', (request, response) => {
+    response.send(appData);
 });
+
+const addMovie = (req, res) => {
+    console.log(req.body);
+    appData.push(req.body);
+};
+
+// Basic POST request
+app.post('/addMovie', addMovie);
+
+app.use(express.static('website'));
 
 const server = app.listen(port, () => {
     console.log(`Running on localhost: ${port}`);
